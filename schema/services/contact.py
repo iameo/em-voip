c8e0eb91907fac575c507bc15a3fd703ceaec4d9
PPE <okwudike.emmanuel@gmail.com>
2022-09-01 14:07:19 +0200

    docstring included

--- schema/services/contact.py
+++ schema/services/contact.py
@@ -5,16 +5,43 @@ import re
 
 
 class ContactNumber(BaseModel):
+    """
+    ContactNumber is the base model for which other schema inherints from:
+
+    args:
+        is_shared: if the resource is shared (default: 0)
+        is_primary: if the purchased phone number is primary contact (default: 0)
+        area_code: the area code associated with the phone number (default: '')
+    """
     is_shared: int
     is_primary: int
     area_code: str
 
 
 class ContactNumberPost(ContactNumber):
-    country: str
+    """
+    Schema for purchasing a phone number
+    
+    args necessary for allocating a number: area_code and country
+
+    args:
+        country: country where to purchase number from (default: United States)
+    """
+    country: str = 'US'
 
 
 class ContactNumberPut(ContactNumberPost):
+    """
+    Schema post number buying (PUT request)
+    
+    forwarding_enabled: activate call forwarding on a number (default: 0)
+    forward_to: if call forwarding is enabled, the contact call is forwarded to
+    voice_greeting_enabled: activate voice mail greeting (default: 0)
+    voice_greeting_url: (pre)-recorded messages will be converted to an url if voice greeting is enabled'
+    label: phone number label
+
+    """
+
     forwarding_enabled: int
     forward_to: Optional[str]
     voice_greeting_enabled: int
@@ -22,7 +49,7 @@ class ContactNumberPut(ContactNumberPost):
     label: Optional[str]
 
     @validator('forward_to')
-    def validate_xxx(cls, v, values, **kwargs):
+    def validate_forwarding(cls, v, values, **kwargs):
         forwarding_enabled = values.get('forwarding_enabled')
         phone_regex = re.compile("^(\+\d{1,3})?[-\s. ]*\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$", re.IGNORECASE)
         contact_validity = phone_regex.match(v)
@@ -36,6 +63,6 @@ class ContactNumberPut(ContactNumberPost):
     def validate_voice_greeting(cls, v, values, **kwargs):
         is_greeting = values.get('voice_greeting_enabled')
         if is_greeting and v == '':
-            raise ValueError("Voice Greeting is enabled. Kindly record your message.")
+            raise ValueError("Voice Greeting is enabled. Kindly record your message so we can establish voicemail greeting.")
         return v
 
