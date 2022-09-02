d8f8f72c0cea579b9d6093a98f0e77c3a35ef203
PPE <okwudike.emmanuel@gmail.com>
2022-09-02 23:36:08 +0200

    starter config

---
+++ config.py
@@ -0,0 +1,17 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+class Config(object):
+    CLIENT_ID= os.getenv("CLIENT_ID")
+    CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+
+    SECRET_KEY = os.getenv('jwt_key')
+    JWT_SECRET_KEY = os.getenv('jwt_key')
+    PROPAGATE_EXCEPTIONS = True
+
+    ALLOWED_SOCKET_ORIGINS = "*"
+    DEBUG = os.getenv('DEBUG', False)
+
