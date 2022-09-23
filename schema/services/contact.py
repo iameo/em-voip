92cead4c3b422215980050b9c2511f185d24d9e7
PPE <okwudike.emmanuel@gmail.com>
2022-09-23 02:40:50 +0200

    prevent injection and updated validator on pre loading

--- schema/services/contact.py
+++ schema/services/contact.py
@@ -1,7 +1,7 @@
 from typing import Optional, Union
 from pydantic import BaseModel, validator
 import re
-
+from services.middlewares.common import clean_payload
 
 
 class ContactNumber(BaseModel):
@@ -16,6 +16,10 @@ class ContactNumber(BaseModel):
     is_shared: int
     is_primary: int
     area_code: str
+
+    @validator("*", pre=True)
+    def clean(cls, value):
+        return clean_payload(value)
 
 
 class ContactNumberPost(ContactNumber):
@@ -43,17 +47,18 @@ class ContactNumberPut(ContactNumberPost):
     """
 
     forwarding_enabled: int
-    forward_to: Optional[str]
+    forward_to: Optional[str] = ''
     voice_greeting_enabled: int
-    voice_greeting_url: Optional[str]
+    voice_greeting_url: Optional[str] = ''
     label: Optional[str]
 
     @validator('forward_to')
     def validate_forwarding(cls, v, values, **kwargs):
         forwarding_enabled = values.get('forwarding_enabled')
+
         phone_regex = re.compile("^(\+\d{1,3})?[-\s. ]*\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$", re.IGNORECASE)
         contact_validity = phone_regex.match(v)
-        if forwarding_enabled and (v == '' or contact_validity is None):
+        if (forwarding_enabled == 1) and (v == '' and contact_validity is None):
             input_condition = 'Empty value' if not v else 'Input %s' % str(v)
             raise ValueError(f"Forwarding is enabled. {input_condition} is not a valid phone number.")
         return v
@@ -62,7 +67,7 @@ class ContactNumberPut(ContactNumberPost):
     @validator('voice_greeting_url')
     def validate_voice_greeting(cls, v, values, **kwargs):
         is_greeting = values.get('voice_greeting_enabled')
-        if is_greeting and v == '':
+        if (is_greeting == 1) and v == '':
             raise ValueError("Voice Greeting is enabled. Kindly record your message so we can establish voicemail greeting.")
         return v
 
