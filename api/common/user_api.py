d81d1f1af79602d44842df15ce419df3d6eb2913
PPE <okwudike.emmanuel@gmail.com>
2023-01-25 21:33:32 +0100

    user api modified to house registration and proper login

--- api/common/user_api.py
+++ api/common/user_api.py
@@ -1,7 +1,7 @@
 from flask import request
 from db import db_field, db_save
 from model.response import response_model
-from flask_restx import Resource
+from flask_restx import Resource, Namespace
 
 import bcrypt
 import os
@@ -10,8 +10,23 @@ from utils.exts import random_label
 
 from datetime import datetime
 
+from flask_restx.reqparse import RequestParser
+from flask_restx.inputs import email
+
+
+auth_ns = Namespace(name="auth", validate=True)
+
 users_index = os.getenv('USERS_INDEX', 'users')
 
+
+
+auth_reqparser = RequestParser(bundle_errors=True)
+auth_reqparser.add_argument(
+    name="email", type=email(), location="form", required=True, nullable=False
+)
+auth_reqparser.add_argument(
+    name="password", type=str, location="form", required=True, nullable=False
+)
 
 def allow_login(json_data):
     email = json_data.get('email')
@@ -28,23 +43,31 @@ def allow_login(json_data):
             access_token = create_access_token(identity={
                 'name': response_['account_data'].get('name'),
                 'email': response_.get('email'),
-                'user_id': response_.get('user_id')
+                'user_id': response_.get('user_id'),
+                'account_id': response_['account_data'].get('account_id')
             }, expires_delta=False)
 
             data = {
                 'token': access_token,
                 'email': response_.get('email'),
+                'status': 200,
+                'message': 'User found.',
                 'kw': response_
             }
-
             return data
-    return {"data":{}, "message":"failed"}
+        return {"data":{}, "message":"user could not be validated. kindly check the password and try again", "status":401}
+    return {"data":{}, "message":"this user could not be found. kindly check the email and try again", "status":404}
+
 
 class AllowLogin(Resource):
+    @auth_ns.expect(auth_reqparser)
     def post(self):
         auth_data = request.get_json()
         resp = allow_login(auth_data)
-        return response_model(allow_only_data=True, response=resp)
+        status = str(resp.pop('status'))
+        if '200' in status:
+            return response_model(allow_only_data=True, response=resp)
+        return response_model(response=resp, explicit_message='User could not be found')
 
 
 
@@ -69,13 +92,12 @@ def register_user(json_data):
                     'name': name,
                     'email': email,
                     'user_id': user_id,
+                    'account_id': account_id
                 }, expires_delta=False)
 
         _password = bcrypt.hashpw(b_password, salt)
 
         current_time = datetime.now()
-
-
 
         data = {
             "status": "active",
