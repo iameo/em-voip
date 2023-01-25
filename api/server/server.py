6ec9337e2eccf11c3449d9ae9df2002a02c17dd6
PPE <okwudike.emmanuel@gmail.com>
2023-01-25 21:38:18 +0100

    server updated

--- api/server/server.py
+++ api/server/server.py
@@ -1,11 +1,17 @@
 from flask_restx import Resource
 from flask import jsonify
 from flask_api import status as _status
-
+from routes.restx_loader import restx_api
+from http import HTTPStatus
+from flask_jwt_extended import jwt_required
 
 class Index(Resource):
+    @restx_api.doc(security='Bearer')
+    @restx_api.response(int(HTTPStatus.BAD_REQUEST), "Validation error.")
+    @restx_api.response(int(HTTPStatus.UNAUTHORIZED), "Token is invalid or expired.")
+    @jwt_required()
     def get(self):
         return jsonify({
                 "status": _status.HTTP_200_OK,
-                "status": "fetched successfully",
+                "status": "fetchedd successfully",
             })
