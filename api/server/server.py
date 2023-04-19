87c0d92cf0b39ca28357facc1ed6cd5dec716926
PPE <okwudike.emmanuel@gmail.com>
2023-04-19 16:18:09 +0100

    FFA + updated response key

--- api/server/server.py
+++ api/server/server.py
@@ -1,17 +1,13 @@
 from flask_restx import Resource
 from flask import jsonify
 from flask_api import status as _status
-from routes.restx_loader import restx_api
-from http import HTTPStatus
-from flask_jwt_extended import jwt_required
 
 class Index(Resource):
-    @restx_api.doc(security='Bearer')
-    @restx_api.response(int(HTTPStatus.BAD_REQUEST), "Validation error.")
-    @restx_api.response(int(HTTPStatus.UNAUTHORIZED), "Token is invalid or expired.")
-    @jwt_required()
+    """
+    Index endpoint: AnonymousUser allowed; just to check server is running
+    """
     def get(self):
         return jsonify({
                 "status": _status.HTTP_200_OK,
-                "status": "fetchedd successfully",
+                "message": "fetchedd successfully",
             })
