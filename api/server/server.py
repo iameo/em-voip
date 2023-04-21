bfe03d7383201477df34c17ce9a2e23a2a920bce
PPE <okwudike.emmanuel@gmail.com>
2023-04-21 16:19:41 +0100

    Bearer + get_jwt_identity()

--- api/server/server.py
+++ api/server/server.py
@@ -1,13 +1,16 @@
 from flask_restx import Resource
 from flask import jsonify
 from flask_api import status as _status
+from flask_jwt_extended import jwt_required, get_jwt_identity
+from routes.restx_loader import restx_api
 
 class Index(Resource):
-    """
-    Index endpoint: AnonymousUser allowed; just to check server is running
-    """
+    @restx_api.doc(security="Bearer")
+    @jwt_required()
     def get(self):
+        """ Index endpoint: authenticared users only; checks server is running"""
         return jsonify({
                 "status": _status.HTTP_200_OK,
                 "message": "fetched successfully",
+                "extra": get_jwt_identity()
             })
