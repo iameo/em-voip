fa8bbd7b50d7b42faa108c7a5987518e2a0a1300
PPE <okwudike.emmanuel@gmail.com>
2023-04-21 16:22:20 +0100

    voip naming

--- routes/restx_loader.py
+++ routes/restx_loader.py
@@ -2,4 +2,4 @@ from flask_restx import Api
 
 restx_api = Api(authorizations = {"Bearer": {"type": "apiKey", "in": "header", "name": "Authorization"}})
 
-ns = restx_api.namespace('voip', description='Voice Over Internet Protocols')
+ns = restx_api.namespace('voip', description='Voice Over Internet Protocols \n Register => Login => Enter token as Bearer $token')
